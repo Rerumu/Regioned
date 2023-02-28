6a59d00ebdad592c2bdd7936ba53a54ea0e57451
Rerumu <25379555+Rerumu@users.noreply.github.com>
2023-02-28 03:34:56 -0500

    Refactor `MarkAndSweep` using `Walker`

--- src/transform/mark_and_sweep.rs
+++ src/transform/mark_and_sweep.rs
@@ -1,11 +1,10 @@
-use std::collections::HashSet;
-
 use crate::data_flow::{graph::Graph, node::NodeId};
+
+use super::walker::Walker;
 
 #[derive(Default)]
 pub struct MarkAndSweep {
-	marked: HashSet<NodeId>,
-	boundary: Vec<NodeId>,
+	walker: Walker,
 }
 
 impl MarkAndSweep {
@@ -15,47 +14,18 @@ impl MarkAndSweep {
 		Self::default()
 	}
 
-	fn mark_node_at(&mut self, id: NodeId) {
-		if self.marked.contains(&id) {
-			return;
-		}
-
-		self.marked.insert(id);
-		self.boundary.push(id);
-	}
-
-	fn mark_links_at<S>(&mut self, graph: &Graph<S>, id: NodeId) {
-		if graph.nodes[id].as_compound().is_some() {
-			for region in &graph.regions[id] {
-				self.mark_node_at(region.start());
-				self.mark_node_at(region.end());
-			}
-		}
-
-		for link in &graph.predecessors[id] {
-			self.mark_node_at(link.node());
-		}
-	}
-
-	fn mark<S>(&mut self, graph: &Graph<S>, roots: &[NodeId]) {
-		for &root in roots {
-			self.mark_node_at(root);
-		}
-
-		while let Some(id) = self.boundary.pop() {
-			self.mark_links_at(graph, id);
-		}
-	}
-
 	fn sweep<S>(&self, graph: &mut Graph<S>) {
-		graph.nodes.retain(|id, _| self.marked.contains(&id));
+		let seen = self.walker.seen();
+
+		graph.nodes.retain(|id, _| seen.contains(&id));
 	}
 
 	/// Runs the mark-and-sweep algorithm on the given [`Graph`].
-	pub fn run<S>(&mut self, graph: &mut Graph<S>, roots: &[NodeId]) {
-		self.marked.clear();
-
-		self.mark(graph, roots);
+	pub fn run<S, I>(&mut self, graph: &mut Graph<S>, roots: I)
+	where
+		I: IntoIterator<Item = NodeId>,
+	{
+		self.walker.run(graph, roots);
 		self.sweep(graph);
 	}
 }
