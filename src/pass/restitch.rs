73a448bf61264937ff28aabe48320d046a7bb879
Rerumu <25379555+Rerumu@users.noreply.github.com>
2023-04-05 02:25:52 -0400

    Add `Port` redo functions for restitching

--- src/pass/restitch.rs
+++ src/pass/restitch.rs
@@ -1,7 +1,42 @@
 use crate::data_flow::{
-	graph::Graph,
+	graph::{Graph, PredecessorList},
+	link::{Link, Port},
 	node::{Id, Node},
 };
+
+use super::successors::Successors;
+
+/// Redoes the ports of the successors of the node `from` to point to the node `to`.
+/// The ports are updated using the function `redo`.
+pub fn redo_ports<M>(
+	predecessors: &mut [PredecessorList],
+	successors: &Successors,
+	from: Id,
+	to: Id,
+	redo: M,
+) where
+	M: Fn(Port) -> Option<Port>,
+{
+	let relevant = |predecessor: &&mut Link| predecessor.node() == from;
+
+	for &successors in &successors.cache()[&from] {
+		for predecessor in predecessors[successors].iter_mut().filter(relevant) {
+			if let Some(port) = redo(predecessor.port()) {
+				*predecessor = Link::new(to, port);
+			}
+		}
+	}
+}
+
+/// Redoes the ports of the successors of the node `from` to point to the node `to`.
+pub fn redo_ports_in_place(
+	predecessors: &mut [PredecessorList],
+	successors: &Successors,
+	from: Id,
+	to: Id,
+) {
+	redo_ports(predecessors, successors, from, to, Some);
+}
 
 /// Applies the rule `applier` to the graph nodes.
 /// If the rule succeeds, the result is passed to `stitcher` and the node is updated.
