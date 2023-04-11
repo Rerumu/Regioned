03fd6edc960381fbae7f3fc7d4bad176bc2aeba8
Rerumu <25379555+Rerumu@users.noreply.github.com>
2023-04-11 19:56:10 -0400

    Chop down `MarkAndSweep` into one function

--- src/pass/mark_and_sweep.rs
+++ src/pass/mark_and_sweep.rs
@@ -2,33 +2,15 @@ use crate::data_flow::{graph::Graph, node::Id};
 
 use super::traverse::reverse_topological::ReverseTopological;
 
-/// A mark-and-sweep algorithm for removing unreachable nodes from the graph.
-/// It uses a shallow traversal which may result in logically unreachable nodes being retained.
-#[derive(Default)]
-pub struct MarkAndSweep {
-	topological: ReverseTopological,
-}
+/// Marks all reachable nodes from the given roots, then removes all unmarked nodes.
+pub fn run<S, I>(graph: &mut Graph<S>, roots: I, topological: &mut ReverseTopological)
+where
+	I: IntoIterator<Item = Id>,
+{
+	topological.run(graph, roots);
 
-impl MarkAndSweep {
-	/// Creates a new, reusable [`MarkAndSweep`] instance.
-	#[must_use]
-	pub fn new() -> Self {
-		Self::default()
-	}
+	let seen = topological.seen();
 
-	fn sweep<S>(&self, graph: &mut Graph<S>) {
-		let seen = self.topological.seen();
-
-		graph.nodes.retain(|id, _| seen[id]);
-		graph.regions.retain(|id, _| seen[*id]);
-	}
-
-	/// Removes unreachable nodes from the graph.
-	pub fn run<S, I>(&mut self, graph: &mut Graph<S>, roots: I)
-	where
-		I: IntoIterator<Item = Id>,
-	{
-		self.topological.run(graph, roots);
-		self.sweep(graph);
-	}
+	graph.nodes.retain(|id, _| seen[id]);
+	graph.regions.retain(|id, _| seen[*id]);
 }
